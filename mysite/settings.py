17753e71a7b4723f8375e78b92789070ee05629f
GY L <31459800+Little-G@users.noreply.github.com>
2017-09-18 15:00:41 +1000

    no message

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,3 +81,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'freeAgent/static'),
+)
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+TEMPLATES = [
+    {
+      'BACKEND': 'django.template.backends.django.DjangoTemplates',
+      'DIRS': [os.path.join(BASE_DIR, '/freeAgent/templates/').replace('\\', '/'), ],
+      'APP_DIRS': True,
+      'OPTIONS': {
+          'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+
