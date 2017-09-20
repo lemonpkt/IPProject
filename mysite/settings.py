67c96568a4ee408813168e04c5649f4382ea100a
GY L <31459800+Little-G@users.noreply.github.com>
2017-09-20 13:30:43 +1000

    no message

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-	'freeAgent.apps.FreeAgentConfig',
+	'freeAgent',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
