7e42f7f29ae362a29ab3bc1e15724139fe88b829
HAL <leesy@live.com.au>
2017-09-22 22:22:58 +1000

    Added Login Buttons, Changed views to show register & end_client_project

--- freeAgent/urls.py
+++ freeAgent/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     url(r'^all_projects$', views.all_projects.as_view(), name='all_projects'),
     url(r'^create_projects$', views.create_project.as_view(), name='create_project'),
     url(r'^end_client_projects$', views.end_client_projects.as_view(), name='end_client_projects'),
-    url(r'^$', views.login.as_view(), name='login'),
-    url(r'^register$', views.register.as_view(), name='register')
-
-    
+    url(r'^login$', views.login.as_view(), name='login'),
+    url(r'^register$', views.register.as_view(), name='register'),
+   
 ]
--- freeAgent/views.py
+++ freeAgent/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import Http404
-from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from .models import Project, Member, Review
 from django.views import generic
-
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class accepted_projects(generic.ListView):
@@ -18,7 +16,7 @@ class accepted_projects(generic.ListView):
     
     def get_queryset(self):
         """Returns accepted Projects that belong to the current user"""
-        return Project.objects.filter(user=self.request.user_name,statsu=2).order_by('project_id')
+        return Project.objects.filter(status=2).order_by('project_id')
                    
             
 class all_projects(generic.ListView):
@@ -27,7 +25,7 @@ class all_projects(generic.ListView):
     
     def get_queryset(self):
         """Return all projects"""
-        return Project.objects.order_by("pub_date")
+        return Project.objects.order_by('pub_date')
     
     
 class create_project(generic.ListView):
@@ -43,7 +41,7 @@ class end_client_projects(generic.ListView):
     
     def get_queryset(self):
         """Return all proejcts owned by a client"""
-        return Project.objects.filter(client.self)
+        return Project.objects.order_by('project_id')
     
     
 class login(generic.ListView):
