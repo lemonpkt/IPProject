c7da6749e52cd8431703d3e036c3f75ada49809b
GY L <31459800+Little-G@users.noreply.github.com>
2017-10-18 14:28:30 +1100

    finish check user name is existed by Ajax

--- freeAgentApp/forms.py
+++ freeAgentApp/forms.py
@@ -1,3 +1,5 @@
+
+
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UsernameField
--- freeAgentApp/static/freeAgentApp/js/checkUsername.js
+++ freeAgentApp/static/freeAgentApp/js/checkUsername.js
@@ -1,22 +1,8 @@
-$(function(){
-    var username = $('.id_username').val();
-    var $pTag = document.getElementById('pTag-username');
+
+
+var $pTag = document.getElementById('pTag-username');
 
     function exist() {
         $pTag.innerHTML = 'User Name existed!'
     }
 
-    $.ajax({
-        type:'get',
-        url:'freeAgentApp/serializerUsername',
-        dataType:'json',
-        success: function (user) {
-            for(var i = 0; i < user.length; i++){
-                if (user[i].username = username){
-                    exist();
-                    break;
-                }
-            }
-        }
-    })  
-});
