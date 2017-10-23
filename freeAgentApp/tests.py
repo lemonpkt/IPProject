7da1acf2d6bd103021964b1507b409f29fb33a59
Alex Mueller <2556429+lemonpkt@users.noreply.github.com>
2017-10-23 13:30:02 +1100

    Test login page redirect based on code from @GilliusThunderHead

--- freeAgentApp/tests.py
+++ freeAgentApp/tests.py
@@ -1,3 +1,22 @@
+import datetime
+from django.utils import timezone
 from django.test import TestCase
+from freeAgentApp.models import Project,Review,UserProfile
+from django.test.utils import setup_test_environment
+from django.test import Client
+from django.urls import reverse
+from django.db import models
 
-# Create your tests here.
+
+class ProjectModelTests(TestCase):
+    
+    client = Client()
+    
+    # def user_is_unique(self):
+        # """user_is_unique() returns False for user registration if username is not unique."""
+       
+    def test_user_is_logged_in(self):
+        """Checks that user is redirected to login page if not in a current session"""
+        response = self.client.get(reverse('freeAgentApp:index'))
+        self.assertEqual(response.status_code, 302)
+
