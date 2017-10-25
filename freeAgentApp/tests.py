78457252492da59e11a92a0be9959495d7780e88
Alex Mueller <2556429+lemonpkt@users.noreply.github.com>
2017-10-25 17:04:20 +1100

    Added test for creating a project

--- freeAgentApp/tests.py
+++ freeAgentApp/tests.py
@@ -1,12 +1,9 @@
-import datetime
-from django.utils import timezone
-from django.test import TestCase
-from freeAgentApp.models import Project,Review,UserProfile
-from django.test.utils import setup_test_environment
+from django.test import TestCase, RequestFactory
 from django.test import Client
 from django.urls import reverse
-from django.db import models
 
+from freeAgentApp.models import Project, Review, UserProfile
+from freeAgentApp.views import ProjectCreate
 
 class ProjectModelTests(TestCase):
     
@@ -19,4 +16,30 @@ class ProjectModelTests(TestCase):
         """Checks that user is redirected to login page if not in a current session"""
         response = self.client.get(reverse('freeAgentApp:index'))
         self.assertEqual(response.status_code, 302)
+
+class ClientCreateProjectTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = UserProfile.objects.get_or_create(username='test_user')[0]
+
+        # Create a logged-in client
+        self.client = Client()
+        self.client.force_login(self.user)
+
+    def test_create_project(self):
+        """Checks that when a client creates a new project they are added as
+        the owner of that project.
+
+        """
+        # Send a form to the createProject page
+        response = self.client.post(reverse('freeAgentApp:createProject'),
+                {'title': "My Cool Project",
+                    'cost': '999.50',
+                    'description': "This is a cool project for cool dudes.",
+                    'file_type': ''})
+        #self.assertEqual(response.status_code, 200)
+
+        # Check that there is a new Project in the db pointing to self.user
+        self.assertEqual(len(Project.objects.filter(client=self.user)), 1)
+
 
