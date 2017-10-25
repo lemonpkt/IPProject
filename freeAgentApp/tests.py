c43265388bd0b016703501f1fda21b9f2bb95203
Alex Mueller <2556429+lemonpkt@users.noreply.github.com>
2017-10-25 17:43:22 +1100

    Cleaned up tests

--- freeAgentApp/tests.py
+++ freeAgentApp/tests.py
@@ -5,15 +5,13 @@ from django.urls import reverse
 from freeAgentApp.models import Project, Review, UserProfile
 from freeAgentApp.views import ProjectCreate
 
-class ProjectModelTests(TestCase):
+class LoginTests(TestCase):
     
     client = Client()
     
-    # def user_is_unique(self):
-        # """user_is_unique() returns False for user registration if username is not unique."""
-       
-    def test_user_is_logged_in(self):
+    def test_required_login(self):
         """Checks that user is redirected to login page if not in a current session"""
+
         response = self.client.get(reverse('freeAgentApp:index'))
         self.assertEqual(response.status_code, 302)
 
@@ -36,6 +34,7 @@ class ProjectModelTests(TestCase):
         is already taken.
         
         """
+
         username = 'unique_username'
 
         user = UserProfile.objects.create(username=username)
@@ -49,7 +48,7 @@ class ProjectModelTests(TestCase):
         self.assertNotEqual(response.status_code, 302)
 
 
-class ClientCreateProjectTest(TestCase):
+class ClientTests(TestCase):
     def setUp(self):
         # Create a test user
         self.user = UserProfile.objects.get_or_create(username='test_user')[0]
@@ -63,13 +62,13 @@ class ClientCreateProjectTest(TestCase):
         the owner of that project.
 
         """
+
         # Send a form to the createProject page
         response = self.client.post(reverse('freeAgentApp:createProject'),
                 {'title': "My Cool Project",
                     'cost': '999.50',
                     'description': "This is a cool project for cool dudes.",
                     'file_type': ''})
-        #self.assertEqual(response.status_code, 200)
 
         # Check that there is a new Project in the db pointing to self.user
         self.assertEqual(len(Project.objects.filter(client=self.user)), 1)
